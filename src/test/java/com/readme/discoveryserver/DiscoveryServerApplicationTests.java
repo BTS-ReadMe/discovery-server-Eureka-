47e3d41fce13c2a27e04ace9e4838dfde0e43c33
Ju-Yeongmin <drodudals1@gmail.com>
2023-04-28 09:26:46 +0900

    first commit

---
+++ src/test/java/com/readme/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readme.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
